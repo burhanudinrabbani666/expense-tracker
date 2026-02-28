1b42951bc17e38be7a62da5a3386ae1f4415266e
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-02-28 13:26:38 +0700

    feat: implement  edit

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ const { description, amount, id, month } = program.opts();
 const [action] = program.args;
 
 if (action === "add") handleAddExpense(description, amount);
-if (action === "update") handleUpdate(id, amount, description);
-if (action === "delete") handleDelete(id);
-if (action === "list") handleList();
-if (action === "summary") handleSummary(month);
-
+else if (action === "update") handleUpdate(id, amount, description);
+else if (action === "delete") handleDelete(id);
+else if (action === "list") handleList();
+else if (action === "summary") handleSummary(month);
+else console.log("Action sould be one of add,edit,list or summary");
 // -------------------------------------------------------------------
 
 function getData() {
@@ -46,24 +46,51 @@ function handleAddExpense(description, amount) {
   const expenseData = getData();
 
   const newExpense = {
-    id: expenseData.length > 0 ? expenseData.at(-1).id + 1 : 1,
-    expenseDescrption: description,
+    id: expenseData.length > 0 ? expenseData[0].id + 1 : 1,
+    expenseDescription: description,
     amount,
     createdAt: new Date().toLocaleString("en-US", DATE_OPTION),
     updatedAt: null,
   };
 
-  const newExpenseData = [newExpense, ...expenseData];
+  expenseData.unshift(newExpense);
 
-  fs.writeFileSync(pathFile, JSON.stringify(newExpenseData), (error) => {
-    if (error) return console.log(error);
-  });
-
+  try {
+    fs.writeFileSync(pathFile, JSON.stringify(expenseData));
+  } catch (error) {
+    console.log(error);
+  }
   return;
 }
 
 function handleUpdate(id, amount, description) {
-  console.log(id, amount, description);
+  if (!id) return console.log("edit must be insert id");
+
+  const expenseData = getData();
+  const expenseToEdit = expenseData.find((expense) => expense.id === id);
+
+  if (!expenseToEdit) return console.log("Expense not Found!");
+
+  const newExpense = {
+    id,
+    expenseDescription: description
+      ? description
+      : expenseToEdit.expenseDescription,
+    amount: amount ? amount : expenseToEdit.amount,
+    createdAt: expenseToEdit.createdAt,
+    updatedAt: new Date().toLocaleString("en-US", DATE_OPTION),
+  };
+
+  const newExpenseData = expenseData.map((expense) =>
+    expense.id === newExpense.id ? newExpense : expense,
+  );
+
+  try {
+    fs.writeFileSync(pathFile, JSON.stringify(newExpenseData));
+  } catch (error) {
+    console.log(error);
+  }
+  return;
 }
 
 function handleDelete(id) {
@@ -71,22 +98,23 @@ function handleDelete(id) {
 
   const expenseData = getData();
 
-  if (expenseData.length < 0)
+  if (expenseData.length === 0)
     return console.log("You dont have expense to track!");
 
   const newExpenseData = expenseData.filter((expense) => expense.id !== id);
 
-  fs.writeFileSync(pathFile, JSON.stringify(newExpenseData), (error) => {
-    if (error) return console.log(error);
-  });
-
+  try {
+    fs.writeFileSync(pathFile, JSON.stringify(newExpenseData));
+  } catch (error) {
+    console.log(error);
+  }
   return;
 }
 
 function handleList() {
   const expenseData = getData();
 
-  if (expenseData.length < 0)
+  if (expenseData.length === 0)
     return console.log("You dont have expense to track!");
 
   console.table(expenseData);
@@ -115,7 +143,7 @@ function handleSummary(month) {
   const monthForSummary = monthArray[month - 1];
   let summary;
 
-  if (month) {
+  if (month !== undefined) {
     const summaryOfMonth = expenseData.filter(
       (expense) => new Date(expense.createdAt).getMonth() === month - 1,
     );
